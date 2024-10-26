01aee9c1deecd753283911c6cb2ab98f90fbe2fe
Ali-Dabiri <dabiri1380@gmail.com>
2024-10-26 15:01:00 +0330

    first commit for News_API_Builder_2

---
+++ News_Pipeline/News_API_Builder_2/News_API_Builder_2/News_Page_App_2/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NewsPageApp2Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'News_Page_App_2'
---
+++ News_Pipeline/News_API_Builder_2/News_API_Builder_2/News_Page_App_2/tests.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from .models import News
+
+class NewsAPITestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        News.objects.create(news_title="Test News 1", news_content="This is test content 1", news_tag="tech", news_source="Source A")
+        News.objects.create(news_title="Test News 2", news_content="This is test content 2", news_tag="sports", news_source="Source B")
+    def test_get_news_list(self):
+        response = self.client.get('/api/news/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
+          
+    def test_filter_by_tag(self):
+        response = self.client.get('/api/news/?tags=tech')
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['news_title'], "Test News 1")
+    
+    def test_filter_by_keywords(self):
+        response = self.client.get('/api/news/?keywords=This')
+        self.assertEqual(len(response.data), 2)
+        self.assertIn(response.data[1]["news_source"], ["Source A", "Source B"])
+    
+    def test_exclude_keywords(self):
+        response = self.client.get('/api/news/?exclude_keywords=content 1')
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["news_title"], "Test News 2")
+
+    def test_filter_by_keywords_and_exclude_keywords(self):
+        response = self.client.get("/api/news/?keywords=content 1&exclude_keywords=content 2")
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["news_title"], "Test News 1") 
